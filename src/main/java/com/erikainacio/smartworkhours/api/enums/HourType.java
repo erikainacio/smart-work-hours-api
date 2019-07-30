4e0192edf3f06702a593a4e459bc76eaa7344ac1
Erika Inacio <einacio@thoughtworks.com>
2019-07-30 19:50:48 -0300

    adding ProfileRole and HourType enums

---
+++ src/main/java/com/erikainacio/smartworkhours/api/enums/HourType.java
@@ -0,0 +1,10 @@
+package com.erikainacio.smartworkhours.api.enums;
+
+public enum HourType {
+    START_WORK,
+    FINISH_WORK,
+    START_LUNCH_BREAK,
+    FINISH_LUNCH_BREAK,
+    START_REGULAR_BREAK,
+    FINISH_REGULAR_BREAK
+}
---
+++ src/main/java/com/erikainacio/smartworkhours/api/enums/ProfileRole.java
@@ -0,0 +1,6 @@
+package com.erikainacio.smartworkhours.api.enums;
+
+public enum ProfileRole {
+    ROLE_ADMIN,
+    ROLE_USER
+}
