53da18ac58589c531f76131a481e219233bd94e3
Erika Inacio <einacio@thoughtworks.com>
2019-07-30 14:39:31 -0300

    initial gradle build

---
+++ src/main/java/com/erikainacio/smartworkhours/api/SmartWorkHoursServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erikainacio.smartworkhours.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWorkHoursServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWorkHoursServiceApplication.class, args);
+	}
+
+}
