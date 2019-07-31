432ef10a93db4fb281e97fa569c9f38b891e27e5
Erika Inacio <einacio@thoughtworks.com>
2019-07-31 12:00:24 -0300

    changing application file name

--- src/main/java/com/erikainacio/smartworkhours/api/SmartWorkHoursApplication.java
+++ src/main/java/com/erikainacio/smartworkhours/api/SmartWorkHoursApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartWorkHoursServiceApplication {
+public class SmartWorkHoursApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartWorkHoursServiceApplication.class, args);
+		SpringApplication.run(SmartWorkHoursApplication.class, args);
 	}
 
 }
